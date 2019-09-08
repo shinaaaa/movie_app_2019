021ba1c5176c782651c1b9da428a2d75f19cfb7f
shin_kim <37543606+shinaaaa@users.noreply.github.com>
2019-09-08 17:29:10 +0900

    site

---
+++ build/precache-manifest.a9c009f9df3092642ee110f89eb9b901.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0877629174f0924b44e5efd458588dfa",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "3f619e821cb01ac61b2b",
+    "url": "/movie_app_2019/static/css/main.9f4f389d.chunk.css"
+  },
+  {
+    "revision": "7c41f6268ddbd4a77207",
+    "url": "/movie_app_2019/static/js/2.70e103f6.chunk.js"
+  },
+  {
+    "revision": "3f619e821cb01ac61b2b",
+    "url": "/movie_app_2019/static/js/main.f8fc56a5.chunk.js"
+  },
+  {
+    "revision": "30a14d7c554eb6584568",
+    "url": "/movie_app_2019/static/js/runtime~main.353f3f61.js"
+  }
+]);
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+/* react Application은 하나의 Component만 Reandering 가능 */
+ReactDOM.render(<App />, document.getElementById('root'));
